5c3e8e192a3070d105d42541257e4eacbeab32bd
suresh <mskmouni@gmail.com>
2025-04-25 17:04:43 +0530

    bhd

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -64,3 +64,16 @@ def test_logina_04(driver):
     login_page.Verify_Remove_btn()
 
 
+@pytest.mark.run(order=-1)
+def test_logina_04(driver):
+    # Load test data
+    login_page = LoginPage(driver)
+    login_page.load_page(data["url"])
+    login_page.enter_username(data["username"])
+    login_page.enter_password(data["password"])
+    login_page.click_login()
+    time.sleep(5)
+    login_page.click_Add_to_cart()
+    login_page.Verify_Remove_btn()
+
+
